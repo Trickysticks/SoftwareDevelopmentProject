5ec645403265d2a9bd24c65390584088af2bdfee
Anthony Calise <anthony.caliseibm.com@anthonys-mbp.armonk.ibm.com>
2020-02-12 11:53:14 -0500

    frontend for w2 form, still need to set correct IDs and Names to components

---
+++ w2.php
@@ -0,0 +1,184 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>Highland Capital Tax System</title>
+  </head>
+  <body>
+    <?php include("navbar.php") ?>
+      <!-- delete later -->
+      <h1 style="text-align:center"> W2 Form </h1>
+    </br></br></br>
+  <form>
+  <div class="form-row">
+    <div class="form-group col-md-4">
+      <label for="inputEmail4">Employer Identifier Number (EIN)</label>
+      <input type="email" class="form-control" id="inputEmail4" >
+    </div>
+    <div class="form-group col-md-4">
+      <label for="inputPassword4">Wages,tips, other compensation</label>
+      <input type="password" class="form-control" id="inputPassword4" >
+    </div>
+    <div class="form-group col-md-4">
+        <label for="inputPassword4">Federal Income Tax Withheld</label>
+        <input type="password" class="form-control" id="inputPassword4" >
+      </div>
+  </div>
+
+<hr>
+  <div class="form-row">
+    <div class="form-group col-md-4">
+      <label for="inputEmail4">Employer's Name</label>
+      <input type="email" class="form-control" id="inputEmail4" >
+
+      <label for="inputEmail4">Employer Address</label>
+      <input type="email" class="form-control" id="inputEmail4" >
+    
+      <label for="inputEmail4">Employer's Zip Code'</label>
+      <input type="email" class="form-control" id="inputEmail4" >
+    
+    </div>
+    <div class="form-group col-md-4">
+      <label for="inputPassword4">Social security wages</label>
+      <input type="password" class="form-control" id="inputPassword4" >
+      <label for="inputPassword4">Medicare wages and tips</label>
+      <input type="password" class="form-control" id="inputPassword4" >
+      <label for="inputPassword4">Social security tips</label>
+      <input type="password" class="form-control" id="inputPassword4" >
+   
+    </div>
+    <div class="form-group col-md-4">
+        <label for="inputPassword4">Social security tax withheld</label>
+        <input type="password" class="form-control" id="inputPassword4" >
+
+        <label for="inputPassword4">Medicare tax withheld</label>
+        <input type="password" class="form-control" id="inputPassword4" >
+     
+        <label for="inputPassword4">Allocated tips</label>
+        <input type="password" class="form-control" id="inputPassword4" >
+     
+      </div>
+     </div>
+</br><hr>
+<div class="form-row">
+        <div class="form-group col-md-4">
+            <label for="inputEmail4">Control number</label>
+            <input type="email" class="form-control" id="inputEmail4" >   
+        </div>
+        <div class="form-group col-md-4">
+            <label for="inputEmail4">Verification code</label>
+            <input type="email" class="form-control" id="inputEmail4" >   
+        </div>
+        <div class="form-group col-md-4">
+            <label for="inputEmail4">Dependent care benefits</label>
+            <input type="email" class="form-control" id="inputEmail4" >   
+        </div>
+    </div>
+</br> <hr>
+  <div class="form-row">
+    <div class="form-group col-md-4">
+      <label for="inputEmail4">Employee's first name</label>
+      <input type="email" class="form-control" id="inputEmail4" >
+      <label for="inputEmail4">Employee's initial</label>
+      <input type="email" class="form-control" id="inputEmail4" >
+      <label for="inputEmail4">Employee's last name</label>
+      <input type="email" class="form-control" id="inputEmail4" >
+      <label for="inputEmail4">Employee's address</label>
+      <input type="email" class="form-control" id="inputEmail4" >
+      <label for="inputEmail4">Employee's zip code</label>
+      <input type="email" class="form-control" id="inputEmail4" >
+   
+    </div>
+    <div class="form-group col-md-4">
+      <label for="inputPassword4">Nonqualified plans</label>
+      <input type="password" class="form-control" id="inputPassword4" >
+    </br></br>
+      <div class="form-row">
+            <div class="btn-group btn-group-toggle" data-toggle="buttons">
+                    <label class="btn btn-secondary active">
+                      <input type="radio" name="options" id="option1" autocomplete="off" checked> Statutory Employee
+                    </label>
+                    <label class="btn btn-secondary">
+                      <input type="radio" name="options" id="option2" autocomplete="off"> Retirement Plan
+                    </label>
+                    <label class="btn btn-secondary">
+                      <input type="radio" name="options" id="option3" autocomplete="off"> Third-party sick pay
+                    </label>
+                  </div>
+            </div>
+        </br></br>
+      <label for="inputPassword4">Other</label>
+      <input type="password" class="form-control" id="inputPassword4" >
+   
+    </div>
+    <div class="form-group col-md-4">
+        <label for="inputPassword4">12a</label>
+        <input type="password" class="form-control" id="inputPassword4" >
+
+        <label for="inputPassword4">12b</label>
+        <input type="password" class="form-control" id="inputPassword4" >
+     
+        <label for="inputPassword4">12c</label>
+        <input type="password" class="form-control" id="inputPassword4" >
+     
+        <label for="inputPassword4">12d</label>
+        <input type="password" class="form-control" id="inputPassword4" >
+     
+      </div>
+     </div>
+</br><hr>
+<div class="form row">
+    <div class="form-group col-md">
+        <label for="inputPassword4">State</label>
+        <input type="password" class="form-control" id="inputPassword4" >
+
+    </div>
+    <div class="form-group col-md">
+        <label for="inputPassword4">Employee's state ID number</label>
+        <input type="password" class="form-control" id="inputPassword4" >
+
+    </div>
+    <div class="form-group col-md">
+        <label for="inputPassword4">State wages, tips, etc</label>
+        <input type="password" class="form-control" id="inputPassword4" >
+
+    </div>
+    <div class="form-group col-md">
+        <label for="inputPassword4">State income tax</label>
+        <input type="password" class="form-control" id="inputPassword4" >
+
+    </div>
+    <div class="form-group col-md">
+        <label for="inputPassword4">Local wages, tips, etc</label>
+        <input type="password" class="form-control" id="inputPassword4" >
+
+    </div>
+    <div class="form-group col-md">
+        <label for="inputPassword4">Local income tip</label>
+        <input type="password" class="form-control" id="inputPassword4" >
+
+    </div>
+    <div class="form-group col-md">
+            <label for="inputPassword4">Locality</label>
+            <input type="password" class="form-control" id="inputPassword4" >    
+
+    </div>
+</div>
+
+
+
+</form>
+
+
+        <br>
+       
+        
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
