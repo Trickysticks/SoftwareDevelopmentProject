f749147a3b5234f435b4c3bce5b1a23a493ec914
wassercb191 <wassermancb@gmail.com>
2020-02-24 20:53:59 -0500

    Updated Index with 941/944

--- index.php
+++ index.php
@@ -31,8 +31,12 @@
                   <a href="w2.php">
                   <button type="button" class="btn-lg btn-dark">Employee W2</button>
                   </a>
+				  <a href="941.php">
                   <button type="button" class="btn-lg btn-dark">941 Form</button>
-                  <button type="button" class="btn-lg btn-dark">944 Form</button>
+				  </a>
+				  <a href="944.php">
+				  <button type="button" class="btn-lg btn-dark">944 Form</button>
+				  </a>
                </div>              
               </div>
         </div>
