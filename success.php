de4729ab74d55d601dee00d3aade60568767bfac
AnthonyCalise <38535420+AnthonyCalise@users.noreply.github.com>
2020-04-02 13:34:47 -0400

    voucher downloading fixed for 941

--- success.php
+++ success.php
@@ -7,7 +7,7 @@ $form941="";
 if (isset($_GET['form944']))
   $form944=$_GET['form944'];
 if (isset($_GET['form941']))
-  $form944=$_GET['form941'];
+  $form941=$_GET['form941'];
 
 ?>
 <!doctype html>
@@ -29,14 +29,13 @@ if (isset($_GET['form941']))
         <h1> Success! You submitted your form </h1></br>
         <a href="index.php"> <button type="button" class="btn-lg btn-dark">Home</button> </a>
         <?php
-        if ($form944=='submitted' || $form941=='submitted')
-            echo '<a href="createFile.php"> <button type="button" class="btn-lg btn-dark">Download Voucher</button> </a>';
+        if ($form944=='submitted')
+            echo '<a href="createFile.php?form944=submitted"> <button type="button" class="btn-lg btn-dark">Download Voucher</button> </a>';
+        elseif ($form941=='submitted')
+            echo '<a href="createFile.php?form941=submitted"> <button type="button" class="btn-lg btn-dark">Download Voucher</button> </a>';
         ?>
        <span>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  </body>
+   </body>
 </html>
 <?php
 
